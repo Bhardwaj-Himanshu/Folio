89971cc29dad072d74dd16868618534a10676df5
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2024-05-08 20:01:46 +0530

    Links added,projects added.Need to add yaml file and ready to deploy!

--- folio.js
+++ folio.js
@@ -3,10 +3,13 @@ let socialMediaLinkHelpTexts = document.querySelectorAll(
   '.social-media-links-help-each'
 );
 let headerButtons = document.querySelectorAll('.button');
+let projects = document.querySelectorAll('.projects');
+let projectLaunchButton = document.querySelectorAll('.project-launch-button');
 
 // Seems like unneccasary creation, nodeList could be traveersed as well.
 let socialMediaButtonsArray = [];
 let socialMediaLinksHelpTextsArray = [];
+let projectsArray = [];
 
 for (let i = 0; i < socialMediaButtons.length; i++) {
   socialMediaButtonsArray.push(socialMediaButtons[i]);
@@ -54,16 +57,64 @@ headerButtons.forEach((button, buttonIndex) => {
       // Remove highlight after 1 second
       setTimeout(() => {
         aboutme.style.color = 'black'; // Reset border to remove highlight
-      }, 1000); // Delay of 1 second
+      }, 500); // Delay of 0.5 second
     } else if (e.target.innerText == 'PROJECTS') {
       //   console.log('projects was clicked');
-      const projectsTrack = document.querySelector('.project-track');
-      projectsTrack.style.border = '5px solid red';
-
+      const projects = document.querySelectorAll('.projects');
+      projects.forEach((project) => {
+        project.style.border = '5px solid red';
+      });
       // Remove highlight after 1 second
       setTimeout(() => {
-        projectsTrack.style.border = '5px solid yellow'; // Reste border to remove highlight
-      }, 1000); // Delay of 1 second
+        projects.forEach((project) => {
+          project.style.border = '5px solid orange';
+        }); // Reste border to remove highlight
+      }, 500); // Delay of 0.5 second
+    }
+  });
+});
+
+projects.forEach((project, projectIndex) => {
+  let isHovered = false; // Track hovered state
+
+  project.addEventListener('mouseenter', () => {
+    if (!isHovered) {
+      // Check if not already hovered
+      isHovered = true; // Set hovered state to true
+      project.classList.add('project-hovered');
+      switch (projectIndex) {
+        case 0:
+          project.innerHTML =
+            '<span>1</span><span>All-In-One</span><a href="https://bhardwaj-himanshu.github.io/All-in-One/" target="_blank" class="project-launch-button">Launch</a>';
+          break;
+        case 1:
+          project.innerHTML =
+            '<span>2</span><span>Landing Page-1</span><a href="https://bhardwaj-himanshu.github.io/MojoClick/" target="_blank" class="project-launch-button">Launch</a>';
+          break;
+        case 2:
+          project.innerHTML =
+            '<span>3</span><span>Landing Page-2</span><a href="https://bhardwaj-himanshu.github.io/EasyBank/" target="_blank" class="project-launch-button">Launch</a>';
+          break;
+        default:
+          break;
+      }
+    }
+  });
+
+  project.addEventListener('mouseleave', () => {
+    project.classList.remove('project-hovered');
+    if (isHovered) {
+      // Check if already hovered
+      isHovered = false; // Set hovered state to false
+      if (projectIndex == 0) {
+        project.textContent = '1';
+      }
+      if (projectIndex == 1) {
+        project.textContent = '2';
+      }
+      if (projectIndex == 2) {
+        project.textContent = '3';
+      }
     }
   });
 });
